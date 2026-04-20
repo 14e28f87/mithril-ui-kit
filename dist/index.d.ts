9f1399c6a4bdd2e56417bf685768c1da6b0aea6e
K.Tsuru <5612289+14e28f87@users.noreply.github.com>
2026-04-20 14:23:16 +0900

    Reorganize standalone repository files

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview
- * Mithril UI Kit - Mithril.js 用 UI コンポーネントライブラリ
- *
- */
-import "bootstrap-icons/font/bootstrap-icons.css";
-export * from "./components/index.js";
-//# sourceMappingURL=index.d.ts.map
